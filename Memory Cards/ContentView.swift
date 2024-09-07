2f359ea0006333cd2781f438f5a85fab87e0a7b4
Registray <72702954+Registray@users.noreply.github.com>
2024-09-07 12:02:56 +0300

    Initial Commit

---
+++ Memory Cards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memory Cards
+//
+//  Created by Oleh Pauk on 07.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
